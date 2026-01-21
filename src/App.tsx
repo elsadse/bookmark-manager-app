7dccc87bc669d1d6c958ba01d493a25a5ac4724b
ElsaDse <elsasifoua02@gmail.com>
2026-01-21 14:06:21 +0100

    Ajout du déploiement avec github pages.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { FormContainerForgotPassword } from '@/components/connexion/FormContaine
 import { FormContainerResetPassword } from '@/components/connexion/FormContainerResetPassword'
 import { FormContainerSignIn } from '@/components/connexion/FormContainerSignIn'
 import { FormContainerSignUp } from '@/components/connexion/FormContainerSignUp'
-import { DarkModeToggle } from '@/DarkModeToggle'
 import { useEffect } from 'react'
 import { Route, Routes, useLocation } from 'react-router'
 
@@ -37,10 +36,10 @@ export function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<FormContainerSignIn />} />
-      <Route path="/signUp" element={<FormContainerSignUp />} />
-      <Route path="/forgotPassword" element={<FormContainerForgotPassword />} />
-      <Route path="/ResetPassword" element={<FormContainerResetPassword />} />
+      <Route path="/bookmark-manager-app/" element={<FormContainerSignIn />} />
+      <Route path="/bookmark-manager-app/signUp" element={<FormContainerSignUp />} />
+      <Route path="/bookmark-manager-app/forgotPassword" element={<FormContainerForgotPassword />} />
+      <Route path="/bookmark-manager-app/ResetPassword" element={<FormContainerResetPassword />} />
     </Routes>
   )
 }
