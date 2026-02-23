c91791a65ab5ab125c9f6dd369baf9e781db5bfe
ElsaDse <elsasifoua02@gmail.com>
2026-02-23 19:25:18 +0100

    Correction bugs dans le ThemeProvider

--- bookmark-manager-ui/src/providers/ThemeProvider.tsx
+++ bookmark-manager-ui/src/providers/ThemeProvider.tsx
@@ -3,27 +3,19 @@ import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { useEffect, type ReactNode } from "react";
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
-    function initialTheme(): Theme {
-        const savedTheme = localStorage.getItem("hs_theme") as Theme | null
-        if (savedTheme === "light" || savedTheme === "dark") {
-            return savedTheme
-        }
-        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-            return "dark"
-        }
-        return "light"
-    }
-
-    const { value, setLocalStorageValue } = useLocalStorage<Theme>("hs_theme", initialTheme())
+    const { value, setLocalStorageValue } = useLocalStorage<Theme>("hs_theme")
 
     useEffect((): void => {
-        if (value === "dark") {
+        const theme: Theme = value ?? (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
+        if (theme === "dark") {
             document.documentElement.classList.add("dark")
         } else {
             document.documentElement.classList.remove("dark")
         }
 
-        setLocalStorageValue(value)
+        if (value) {
+            setLocalStorageValue(theme)
+        }
     }, [value])
 
     return (
